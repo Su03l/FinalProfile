3243569ab47c40637057eb1f75d6457d72e1f104
Suliman Yousef <96296776+Su03l@users.noreply.github.com>
2025-08-11 19:06:31 +0300

    Update project-toggle.js

--- project-toggle.js
+++ project-toggle.js
@@ -1,27 +1,28 @@
-// Project Show More/Less Logic
-function showMoreProjects() {
-  const hiddenProjects = document.querySelectorAll(
-    ".project-card.hidden-project"
-  );
-  hiddenProjects.forEach((card) => {
-    card.style.display = "block";
-  });
-  document.getElementById("showMoreContainer").style.display = "none";
-  document.getElementById("showLessContainer").style.display = "block";
-}
+// // Project Show More/Less Logic
+// function showMoreProjects() {
+//   const hiddenProjects = document.querySelectorAll(
+//     ".project-card.hidden-project"
+//   );
+//   hiddenProjects.forEach((card) => {
+//     card.style.display = "block";
+//   });
+//   document.getElementById("showMoreContainer").style.display = "none";
+//   document.getElementById("showLessContainer").style.display = "block";
+// }
 
-function showLessProjects() {
-  const hiddenProjects = document.querySelectorAll(
-    ".project-card.hidden-project"
-  );
-  hiddenProjects.forEach((card) => {
-    card.style.display = "none";
-  });
-  document.getElementById("showMoreContainer").style.display = "block";
-  document.getElementById("showLessContainer").style.display = "none";
-}
+// function showLessProjects() {
+//   const hiddenProjects = document.querySelectorAll(
+//     ".project-card.hidden-project"
+//   );
+//   hiddenProjects.forEach((card) => {
+//     card.style.display = "none";
+//   });
+//   document.getElementById("showMoreContainer").style.display = "block";
+//   document.getElementById("showLessContainer").style.display = "none";
+// }
 
-// On page load, ensure only 6 projects are visible
-window.addEventListener("DOMContentLoaded", function () {
-  showLessProjects();
-});
+// // On page load, ensure only 6 projects are visible
+// window.addEventListener("DOMContentLoaded", function () {
+//   showLessProjects();
+// });
+
